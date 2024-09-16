90a9082ff26b7a0bafd63ee61d5d296dba2327ed
jchermitano <johnnyhermitano02@gmail.com>
2024-09-16 22:13:06 +0800

    asdf

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
+    win.setGeometry(1200, 300, 700, 700)
+    win.setWindowTitle("Open Lab")
+    win.setWindowIcon(QIcon("logo.png"))
+    win.setToolTip("OpenLab")
     win.show()
     sys.exit(app.exec_())
 
